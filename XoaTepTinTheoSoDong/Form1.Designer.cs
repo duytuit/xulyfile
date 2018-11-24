756cc04c2c7414ea5e0fa35faa1f83b83bf9fc6c
duyttuit <duytu89@gmail.com>
2018-11-24 10:19:44 +0700

    24/11/2018

---
+++ XoaTepTinTheoSoDong/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿using System.Windows.Forms;
+
+namespace XoaTepTinTheoSoDong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnDeleteLine = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.soluongview3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.soluongdelete = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.soluongview4 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.Soluongview1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Soluongview2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Enabled = false;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(37, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 186);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Input File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDeleteLine
+            // 
+            this.btnDeleteLine.Location = new System.Drawing.Point(321, 130);
+            this.btnDeleteLine.Name = "btnDeleteLine";
+            this.btnDeleteLine.Size = new System.Drawing.Size(113, 23);
+            this.btnDeleteLine.TabIndex = 4;
+            this.btnDeleteLine.Text = "Execute";
+            this.btnDeleteLine.UseVisualStyleBackColor = true;
+            this.btnDeleteLine.Click += new System.EventHandler(this.btnDeleteLine_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(446, 46);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(289, 186);
+            this.listBox2.TabIndex = 7;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Enabled = false;
+            this.txtValue.Location = new System.Drawing.Point(321, 104);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(113, 20);
+            this.txtValue.TabIndex = 16;
+            this.txtValue.Text = "%";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(322, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Delete Line To Value:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.soluongview3);
+            this.groupBox2.Controls.Add(this.listBox3);
+            this.groupBox2.Location = new System.Drawing.Point(37, 249);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(338, 288);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Data Before";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Count:";
+            // 
+            // soluongview3
+            // 
+            this.soluongview3.AutoSize = true;
+            this.soluongview3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.soluongview3.ForeColor = System.Drawing.Color.Red;
+            this.soluongview3.Location = new System.Drawing.Point(284, 10);
+            this.soluongview3.Name = "soluongview3";
+            this.soluongview3.Size = new System.Drawing.Size(0, 20);
+            this.soluongview3.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.BackColor = System.Drawing.Color.White;
+            this.listBox3.ForeColor = System.Drawing.Color.Black;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(6, 33);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(326, 251);
+            this.listBox3.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.soluongdelete);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.soluongview4);
+            this.groupBox3.Controls.Add(this.listBox4);
+            this.groupBox3.Location = new System.Drawing.Point(381, 249);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(354, 288);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Data After";
+            // 
+            // soluongdelete
+            // 
+            this.soluongdelete.AutoSize = true;
+            this.soluongdelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.soluongdelete.ForeColor = System.Drawing.Color.Red;
+            this.soluongdelete.Location = new System.Drawing.Point(162, 10);
+            this.soluongdelete.Name = "soluongdelete";
+            this.soluongdelete.Size = new System.Drawing.Size(0, 20);
+            this.soluongdelete.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(183, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Line";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(114, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Deleted:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Count:";
+            // 
+            // soluongview4
+            // 
+            this.soluongview4.AutoSize = true;
+            this.soluongview4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.soluongview4.ForeColor = System.Drawing.Color.Red;
+            this.soluongview4.Location = new System.Drawing.Point(287, 10);
+            this.soluongview4.Name = "soluongview4";
+            this.soluongview4.Size = new System.Drawing.Size(0, 20);
+            this.soluongview4.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(6, 32);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(342, 251);
+            this.listBox4.TabIndex = 1;
+            this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
+            // 
+            // Soluongview1
+            // 
+            this.Soluongview1.AutoSize = true;
+            this.Soluongview1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Soluongview1.ForeColor = System.Drawing.Color.Red;
+            this.Soluongview1.Location = new System.Drawing.Point(170, 18);
+            this.Soluongview1.Name = "Soluongview1";
+            this.Soluongview1.Size = new System.Drawing.Size(0, 20);
+            this.Soluongview1.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Count1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(448, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Count2:";
+            // 
+            // Soluongview2
+            // 
+            this.Soluongview2.AutoSize = true;
+            this.Soluongview2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Soluongview2.ForeColor = System.Drawing.Color.Red;
+            this.Soluongview2.Location = new System.Drawing.Point(492, 18);
+            this.Soluongview2.Name = "Soluongview2";
+            this.Soluongview2.Size = new System.Drawing.Size(0, 20);
+            this.Soluongview2.TabIndex = 24;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(321, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(774, 559);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Soluongview2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Soluongview1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btnDeleteLine);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XuLyFile_v1.0 Factory: Meiko Viet Nam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDeleteLine;
+        private System.Windows.Forms.ListBox listBox2;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private TextBox txtValue;
+        private Label label1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private Label Soluongview1;
+        private Label label2;
+        private Label label3;
+        private Label Soluongview2;
+        private Label label4;
+        private Label soluongview3;
+        private Label label5;
+        private Label soluongview4;
+        private Label soluongdelete;
+        private Label label7;
+        private Label label6;
+        private Button button2;
+    }
+   
+
+}
+
---
+++ XoaTepTinTheoSoDong/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace XoaTepTinTheoSoDong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                DirectoryInfo dinfo = new DirectoryInfo(folderDlg.SelectedPath);
+                FileInfo[] Files = dinfo.GetFiles("*");
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+                foreach (FileInfo file in Files)
+                {
+                     var path = new TestPath(file);
+                    listBox1.Items.Add(path);
+                    Soluongview1.Text = listBox1.Items.Count.ToString();
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            var currentItem = listBox1.SelectedItem as TestPath;
+            if (currentItem != null)
+            {
+                string resuls = currentItem.Original.FullName;
+
+                string[] items = File.ReadAllLines(resuls);
+                foreach (string item in items)
+                {
+                    listBox3.Items.Add(item);
+                    
+                }
+            }
+            else
+            {
+                for (int i = listBox1.SelectedIndices.Count - 1; i >= 0; i--)
+                {
+                    listBox1.Items.RemoveAt(listBox1.SelectedIndices[i]);
+                }
+            }
+            soluongview3.Text = listBox3.Items.Count.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //listBox1.SelectionMode = SelectionMode.MultiExtended;
+        }
+        public class TestPath
+        {
+            public FileInfo Original { get; private set; }
+
+            public TestPath(FileInfo original)
+            {
+                Original = original;
+            }
+
+            public override string ToString()
+            {
+                return Path.GetFileNameWithoutExtension(Original.Name);
+            }
+        }
+
+        private void btnDeleteLine_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string filePath = "";
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                   
+                    listBox1.SetSelected(i, true);
+                    var currentItem = listBox1.SelectedItem as TestPath;
+                    int dongmuonxoa = TotalLines(txtValue.Text.Trim(), currentItem.Original.FullName);
+                    string[] lines = File.ReadAllLines(currentItem.Original.FullName);
+                    List<string> new1 = new List<string>();
+
+                    for (int a = dongmuonxoa; a < lines.Count(); a++)
+                    {
+                        string newlines = lines[a];
+                        new1.Add(newlines);
+                    }
+
+                    //listBox2.DataSource = new1;
+                    //Du lieu la new1---------------->luu vao filePath
+
+                    filePath = folderDlg.SelectedPath + @"\" + currentItem.Original.Name;
+                    StreamWriter objWriter = new StreamWriter(filePath);
+                    for (int b = 0; b < new1.Count(); b++)
+                    {
+                        objWriter.WriteLine(new1[b]);
+                    }
+                    objWriter.Close();
+
+                    listBox1.SetSelected(i, false);
+                }
+                DirectoryInfo dinfo = new DirectoryInfo(folderDlg.SelectedPath);
+                FileInfo[] Files = dinfo.GetFiles("*");
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+                foreach (FileInfo file in Files)
+                {
+                    var path = new TestPath(file);
+                    listBox2.Items.Add(path);
+                    Soluongview2.Text = listBox1.Items.Count.ToString();
+                }
+                listBox2.SetSelected(0, true);
+                MessageBox.Show("Finished!");
+            }
+        }
+        int TotalLines(string valueline,string filePath)
+        {
+            int count =0;
+            string[] lines = File.ReadAllLines(filePath);
+            int linescounttotal = lines.Count();
+            for (int i =0; i < linescounttotal; i++)
+            {
+                
+                if (lines[i].Contains(valueline))
+                {
+                    count = i;
+                    break;
+                }
+            }
+            return count;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            listBox4.Items.Clear();
+            listBox1.SetSelected(listBox2.SelectedIndex, true);
+            if(listBox2.SelectedIndex>=1)
+            {
+                listBox1.SetSelected(listBox2.SelectedIndex-1, false);
+                listBox1.SetSelected(listBox2.SelectedIndex, true);
+              
+            }
+            var currentItem = listBox2.SelectedItem as TestPath;
+            if (currentItem != null)
+            {
+                string resuls = currentItem.Original.FullName;
+
+                string[] items = File.ReadAllLines(resuls);
+                foreach (string item in items)
+                {
+                    listBox4.Items.Add(item);
+                    soluongview4.Text = listBox4.Items.Count.ToString();
+                }
+                soluongdelete.Text = (Convert.ToInt32(soluongview3.Text) - Convert.ToInt32(soluongview4.Text)).ToString();
+            }
+            else
+            {
+                for (int i = listBox2.SelectedIndices.Count - 1; i >= 0; i--)
+                {
+                    listBox2.Items.RemoveAt(listBox2.SelectedIndices[i]);
+                }
+            }
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox3.SetSelected(listBox4.SelectedIndex+Convert.ToInt32(soluongdelete.Text), true);
+            if (listBox4.SelectedIndex >= 1)
+            {
+                listBox3.SetSelected(listBox4.SelectedIndex + Convert.ToInt32(soluongdelete.Text) - 1, false);
+                listBox3.SetSelected(listBox4.SelectedIndex + Convert.ToInt32(soluongdelete.Text), true);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            soluongdelete.Text = null;
+            Soluongview1.Text = null;
+            Soluongview2.Text = null;
+            soluongview3.Text = null;
+            soluongview4.Text = null;
+        }
+    }
+}
